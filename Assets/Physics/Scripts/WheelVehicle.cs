cde03b29c56f8e7b36153a6637f28110b3fc18a1
deadinsaidik228 <164139271+deadinsaidik228@users.noreply.github.com>
2024-10-19 20:20:51 +0500

    Add files via upload

---
+++ Assets/Physics/Scripts/WheelVehicle.cs
@@ -0,0 +1,194 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class WheelVehicle : MonoBehaviour
+{
+    [SerializeField] AnimationCurve turnInputCurve = AnimationCurve.Linear(-1.0f, -1.0f, 1.0f, 1.0f);
+
+    [Header("Wheels")]
+    [SerializeField] WheelCollider[] driveWheel;
+    public WheelCollider[] DriveWheel { get { return driveWheel; } }
+    [SerializeField] WheelCollider[] turnWheel;
+
+    public WheelCollider[] TurnWheel { get { return turnWheel; } }
+    bool isGrounded = false;
+    int lastGroundCheck = 0;
+    public bool IsGrounded
+    {
+        get
+        {
+            if (lastGroundCheck == Time.frameCount)
+                return isGrounded;
+
+            lastGroundCheck = Time.frameCount;
+            isGrounded = true;
+            foreach (WheelCollider wheel in wheels)
+            {
+                if (!wheel.gameObject.activeSelf || !wheel.isGrounded)
+                    isGrounded = false;
+            }
+            return isGrounded;
+        }
+    }
+
+    [Header("Behaviour")]
+    [SerializeField] AnimationCurve motorTorque = new AnimationCurve(new Keyframe(0, 200), new Keyframe(50, 300), new Keyframe(200, 0));
+
+    [Range(2, 16)]
+    [SerializeField] float diffGearing = 4.0f;
+    public float DiffGearing { get { return diffGearing; } set { diffGearing = value; } }
+    [SerializeField] float brakeForce = 1500.0f;
+    public float BrakeForce { get { return brakeForce; } set { brakeForce = value; } }
+
+    [Range(0f, 50.0f)]
+    [SerializeField] float steerAngle = 30.0f;
+    public float SteerAngle { get { return steerAngle; } set { steerAngle = Mathf.Clamp(value, 0.0f, 50.0f); } }
+    [Range(0.001f, 1.0f)]
+    [SerializeField] float steerSpeed = 0.2f;
+    public float SteerSpeed { get { return steerSpeed; } set { steerSpeed = Mathf.Clamp(value, 0.001f, 1.0f); } }
+
+
+    [Range(0.0f, 2f)]
+    [SerializeField] float driftIntensity = 1f;
+    public float DriftIntensity { get { return driftIntensity; } set { driftIntensity = Mathf.Clamp(value, 0.0f, 2.0f); } }
+
+    Vector3 spawnPosition;
+    Quaternion spawnRotation;
+    [SerializeField] Transform centerOfMass;
+    [Range(0.5f, 10f)]
+    [SerializeField] float downforce = 1.0f;
+    public float Downforce { get { return downforce; } set { downforce = Mathf.Clamp(value, 0, 5); } }
+
+    float steering;
+    public float Steering { get { return steering; } set { steering = Mathf.Clamp(value, -1f, 1f); } }
+
+    float throttle;
+    public float Throttle { get { return throttle; } set { throttle = Mathf.Clamp(value, -1f, 1f); } }
+
+    [SerializeField] bool handbrake;
+    public bool Handbrake { get { return handbrake; } set { handbrake = value; } }
+
+    [HideInInspector] public bool allowDrift = true;
+    bool drift;
+    public bool Drift { get { return drift; } set { drift = value; } }
+
+    [SerializeField] float speed = 0.0f;
+    public float Speed { get { return speed; } }
+
+
+    Rigidbody _rb;
+    WheelCollider[] wheels;
+    [SerializeField]  private Vector2 m_movement;
+
+    void Start()
+    {
+
+        _rb = GetComponent<Rigidbody>();
+        spawnPosition = transform.position;
+        spawnRotation = transform.rotation;
+
+        if (_rb != null && centerOfMass != null)
+        {
+            _rb.centerOfMass = centerOfMass.localPosition;
+        }
+
+        wheels = GetComponentsInChildren<WheelCollider>();
+
+        foreach (WheelCollider wheel in wheels)
+        {
+            wheel.motorTorque = 0.0001f;
+        }
+    }
+
+
+    public void Move(InputAction.CallbackContext context)
+    {
+
+        m_movement = context.ReadValue<Vector2>();
+
+    }
+    public float GetSteeringAngle()
+    {
+        return (turnWheel[0].steerAngle / steerAngle);
+    }
+    public void AgentMove(Vector2 _move)
+    {
+
+        m_movement = _move;
+    }
+    public float GetTorque()
+    {
+        return (driveWheel[0].motorTorque / 602f);
+    }
+    void FixedUpdate()
+    {
+        Debug.Log("torque = " + driveWheel[0].motorTorque / 602f);
+        Debug.Log("Steering angle = " + turnWheel[0].steerAngle / steerAngle);
+        speed = transform.InverseTransformDirection(_rb.velocity).z * 3.6f;
+
+
+        throttle = m_movement.y;
+        steering = turnInputCurve.Evaluate(m_movement.x) * steerAngle;
+
+
+        foreach (WheelCollider wheel in turnWheel)
+        {
+            wheel.steerAngle = Mathf.Lerp(wheel.steerAngle, steering, steerSpeed);
+        }
+
+        foreach (WheelCollider wheel in wheels)
+        {
+            wheel.brakeTorque = 0;
+        }
+        if (handbrake)
+        {
+            foreach (WheelCollider wheel in wheels)
+            {
+                wheel.motorTorque = 0.0001f;
+                wheel.brakeTorque = brakeForce;
+            }
+        }else if (Mathf.Abs(speed) < 65 && Mathf.Sign(speed) == Mathf.Sign(throttle))
+        {
+            foreach (WheelCollider wheel in driveWheel)
+            {
+                wheel.motorTorque = throttle * motorTorque.Evaluate(speed) * diffGearing / driveWheel.Length * 2f;
+            }
+        }else
+        {
+            foreach (WheelCollider wheel in wheels)
+            {
+                wheel.brakeTorque = Mathf.Abs(throttle) * brakeForce;
+            }
+        }
+        if (drift && allowDrift)
+        {
+            Vector3 driftForce = -transform.right;
+            driftForce.y = 0.0f;
+            driftForce.Normalize();
+
+            if (steering != 0)
+                driftForce *= _rb.mass * speed / 7f * throttle * steering / steerAngle;
+            Vector3 driftTorque = transform.up * 0.1f * steering / steerAngle;
+
+
+            _rb.AddForce(driftForce * driftIntensity, ForceMode.Force);
+            _rb.AddTorque(driftTorque * driftIntensity, ForceMode.VelocityChange);
+        }
+        _rb.AddForce(-transform.up * speed * downforce);
+    }
+    public void ResetPos()
+    {
+        transform.position = spawnPosition;
+        transform.rotation = spawnRotation;
+
+        _rb.velocity = Vector3.zero;
+        _rb.angularVelocity = Vector3.zero;
+    }
+
+    public void toogleHandbrake(bool h)
+    {
+        handbrake = h;
+    }
+
+}
---
+++ Assets/Scripts/CarAgent.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+
+public class CarAgent : Agent
+{
+
+    [SerializeField] private bool m_allGrounded = false;
+    [SerializeField] private float m_currentReward;
+    [SerializeField] private float m_distanceToTarget;
+    [SerializeField] private float m_prevDistanceToTarget;
+    [SerializeField] private int m_obstacleHit;
+    [SerializeField] private int m_prevObstacleHit;
+    [SerializeField] private int m_nextCheckpointNumber;
+    [SerializeField] private GameObject m_road;
+    [SerializeField] private GameObject m_obstacles;
+    [SerializeField] private Transform m_target;
+    [SerializeField] private Vector3 m_dirToTarget;
+    [SerializeField] private Vector3 m_velocity;
+    [SerializeField] private Vector3 m_angularVelocity;
+
+    private WheelVehicle m_carController;
+    private int m_steps;
+    private int m_deadCounter;
+    private ObstacleGenerator m_obstacleGen;
+    private Rigidbody m_carRigidbody;
+    private RoadGenerator m_roadGen;
+    private Transform m_nextCheckpoint;
+    private Vector2 m_move;
+    private Vector3 m_checkpointPos;
+    private WheelCollider[] m_wheelColliders;
+    private WheelHit m_out;
+
+    public override void Initialize()
+    {
+        m_roadGen = m_road.GetComponent<RoadGenerator>();
+        m_carController = GetComponent<WheelVehicle>();
+        m_carRigidbody = GetComponentInChildren<Rigidbody>();
+        m_wheelColliders = GetComponentsInChildren<WheelCollider>();
+        m_obstacleGen = m_obstacles.GetComponent<ObstacleGenerator>();
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        RoadAndObstacleReset();
+        ResetAll();
+        PrivateVariableReset();
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        m_move.x = Mathf.Clamp(actions.ContinuousActions[0], -1f, 1f);
+        m_move.y = Mathf.Clamp(actions.ContinuousActions[1], -1f, 1f);
+        m_carController.AgentMove(m_move);
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var _actionsOut = actionsOut.ContinuousActions;
+        _actionsOut[0] = m_move.x;
+        _actionsOut[1] = m_move.y;
+        m_carController.AgentMove(m_move);
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        m_velocity = transform.InverseTransformDirection(m_carRigidbody.velocity) / 20f;
+        sensor.AddObservation(new Vector2(m_velocity.x, m_velocity.z)); 
+        sensor.AddObservation(m_distanceToTarget / 30f); 
+        sensor.AddObservation(new Vector2(transform.localPosition.x / 500f, transform.localPosition.z / 500f)); // vec2
+
+        sensor.AddObservation(m_carController.GetTorque()); 
+        sensor.AddObservation(m_carController.GetSteeringAngle()); 
+
+
+        m_dirToTarget = (m_checkpointPos - transform.localPosition).normalized;
+
+        sensor.AddObservation(Vector3.Dot(transform.forward, m_dirToTarget)); 
+
+        m_angularVelocity = transform.InverseTransformDirection(m_carRigidbody.angularVelocity) / 3f;
+        sensor.AddObservation(m_angularVelocity.y); 
+    }
+
+    private void RoadAndObstacleReset()
+    {
+        float _rand = UnityEngine.Random.Range(-1f, 1f);
+
+        m_roadGen.GenTrack((int)(Mathf.Sign(_rand) * Mathf.Ceil(Mathf.Abs(_rand))));
+
+        m_obstacleGen.obstacleState = 1;
+        m_obstacleGen.obstacleDensity = UnityEngine.Random.Range(0.6f, 1.1f);
+        m_obstacleGen.ObstacleStateChange();
+    }
+
+    private void PrivateVariableReset()
+    {
+        m_steps = 0;
+
+        m_carController.AgentMove(Vector2Int.zero);
+
+        m_currentReward = 0;
+
+        m_obstacleHit = 0;
+        m_prevObstacleHit = 0;
+
+        m_deadCounter = 0;
+
+        m_distanceToTarget = Vector3.Distance(m_checkpointPos, transform.localPosition);
+        m_prevDistanceToTarget = m_distanceToTarget;
+    }
+    private void ResetAll()
+    {
+
+        m_nextCheckpoint = m_roadGen.waypoints[0];
+        m_nextCheckpointNumber = 1;
+
+        m_carRigidbody.velocity = Vector3.zero;
+        m_carRigidbody.angularVelocity = Vector3.zero;
+
+        foreach (WheelCollider tempcol in m_wheelColliders)
+        {
+            tempcol.brakeTorque = Mathf.Infinity;
+        }
+
+        transform.localPosition = ((m_roadGen.vertices[4] + m_roadGen.vertices[5] + m_roadGen.vertices[6] + m_roadGen.vertices[7]) / 4f);
+
+        transform.localRotation = Quaternion.LookRotation((((m_nextCheckpoint.localPosition) - new Vector3(0, m_roadGen.halfRoadWidth, 0)) - transform.localPosition).normalized, Vector3.up);
+
+        transform.localPosition += new Vector3(0, 0.8f, 0);
+
+        transform.localPosition += transform.right * UnityEngine.Random.Range((-m_roadGen.halfRoadWidth + 2f), (m_roadGen.halfRoadWidth - 2f));
+
+        transform.Rotate(new Vector3(0f, UnityEngine.Random.Range(-45f, 45f), 0f));
+
+        m_checkpointPos = (new Vector3(m_nextCheckpoint.localPosition.x, transform.localPosition.y, m_nextCheckpoint.localPosition.z));
+
+        m_target.transform.localPosition = ((m_roadGen.vertices[m_roadGen.vertices.Count - 1] + m_roadGen.vertices[m_roadGen.vertices.Count - 2] + m_roadGen.vertices[m_roadGen.vertices.Count - 3] + m_roadGen.vertices[m_roadGen.vertices.Count - 4]) / 4f);
+        m_target.transform.localPosition += new Vector3(0, 0.5f, 0);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+
+        if (collision.collider.CompareTag("Obstacle"))
+        {
+            m_currentReward += -0.01f;
+            AddReward(-0.01f);
+        }
+    }
+        private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("Obstacle"))
+        {
+            m_currentReward += -1f * (m_obstacleHit + 1) * (m_currentReward / 10f);
+            AddReward(-1f * (m_obstacleHit + 1) * (m_currentReward / 10f));
+
+            m_obstacleHit++;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Finish"))
+        {
+            if (m_roadGen.waypoints.Count < m_nextCheckpointNumber)
+            {
+                m_currentReward += 1f + ((30f * m_nextCheckpointNumber) / Mathf.Clamp(m_steps, 1, Mathf.Infinity));
+                AddReward(1f + ((30f * m_nextCheckpointNumber) / Mathf.Clamp(m_steps, 1, Mathf.Infinity)));
+                EndEpisode();
+            }else
+            {
+                NextEpisode(-1f);
+            }
+        }
+
+        else if (other.CompareTag("Waypoint"))
+        {
+
+            if (other.gameObject.transform == m_nextCheckpoint)
+            {
+                m_currentReward += (0.5f + ((20f * m_nextCheckpointNumber) / Mathf.Clamp(m_steps, 1, Mathf.Infinity)));
+                AddReward(0.5f + ((20f * m_nextCheckpointNumber) / Mathf.Clamp(m_steps, 1, Mathf.Infinity)));
+
+                m_prevObstacleHit = m_obstacleHit;
+                NewCheckpoint();
+            }else
+            {
+                m_currentReward += (-1f);
+                AddReward(-1f);
+            }
+        }else if (other.CompareTag("Respawn"))
+        {
+            NextEpisode(-1f);
+        }
+    }
+
+    private void NewCheckpoint()
+    {
+        if (m_roadGen.waypoints.Count > m_nextCheckpointNumber)
+        {
+            m_nextCheckpoint = m_roadGen.waypoints[m_nextCheckpointNumber];
+            m_checkpointPos = (new Vector3(m_nextCheckpoint.localPosition.x, transform.localPosition.y, m_nextCheckpoint.localPosition.z));
+            m_nextCheckpointNumber++;
+
+            m_distanceToTarget = Vector3.Distance(m_checkpointPos, transform.localPosition);
+            m_prevDistanceToTarget = m_distanceToTarget;
+        }else
+        {
+            m_nextCheckpoint = m_target.transform;
+            m_checkpointPos = (new Vector3(m_nextCheckpoint.localPosition.x, transform.localPosition.y, m_nextCheckpoint.localPosition.z));
+            m_nextCheckpointNumber++;
+
+            m_distanceToTarget = Vector3.Distance(m_checkpointPos, transform.localPosition);
+            m_prevDistanceToTarget = m_distanceToTarget;
+        }
+    }
+
+    private void CheckMovement()
+    {
+        CheckDistanceFromCheckpoint();
+
+        if (m_carRigidbody.velocity.magnitude > 1f)
+        { 
+            m_currentReward += (0.0005f * ((Vector3.Dot(m_carRigidbody.velocity.normalized, transform.forward) / 2f) + 0.5f));
+            AddReward((0.0005f * ((Vector3.Dot(m_carRigidbody.velocity.normalized, transform.forward) / 2f) + 0.5f)));
+        }
+        if (m_carRigidbody.velocity.magnitude < 0.5f || !m_allGrounded)
+        {
+            m_deadCounter++;
+        }
+
+        if (m_carRigidbody.velocity.magnitude > 0.5f && m_allGrounded)
+        {
+            m_deadCounter = 0;
+        }
+
+        if (m_deadCounter >= 500)
+        {
+            m_currentReward += -0.001f;
+            AddReward(-0.001f);
+        }
+    }
+        private void CheckDistanceFromCheckpoint()
+    {
+        m_distanceToTarget = Vector3.Distance(m_checkpointPos, transform.localPosition);
+        if (m_steps % 50 == 0)
+        {
+            //Debug.Log("Distance reward = " + (m_prevDistanceToTarget - m_distanceToTarget) / 25f);
+
+            m_currentReward += (m_prevDistanceToTarget - m_distanceToTarget) / 25f;
+            AddReward((m_prevDistanceToTarget - m_distanceToTarget) / 25f);
+
+            m_prevDistanceToTarget = m_distanceToTarget;
+        }
+    }
+    private void CheckGrounded()
+    {
+        foreach (WheelCollider tempcol in m_wheelColliders)
+        {
+            if (transform.position.y >= 1f || transform.position.y <= 0f)
+            {
+                NextEpisode(-1f);
+            }
+
+            if (!tempcol.isGrounded)
+            {
+                m_allGrounded = false;
+                break;
+            }
+
+            else
+            {
+                tempcol.GetGroundHit(out m_out);
+
+                if (m_out.collider.CompareTag("DeadZone"))
+                {
+                    NextEpisode(-1f);
+                }
+            }
+        }
+    }
+        private void NextEpisode(float _reward)
+    {
+        SetReward(_reward);
+        EndEpisode();
+    }
+
+    private void InfiniteRewardCheck()
+    {
+        if (Mathf.Abs(m_currentReward) > 10000)
+        {
+            NextEpisode(0f);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        m_steps++;
+
+        m_allGrounded = true;
+
+        InfiniteRewardCheck();
+        CheckGrounded();
+        CheckMovement();
+    }
+    private void Update()
+    {
+        Debug.Log("Dot product (agent forward,dirToTarget) = "+Vector3.Dot(transform.forward, m_dirToTarget)); 
+    }
+}
